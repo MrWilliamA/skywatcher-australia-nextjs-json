93e45cf01f075d7e46b0250423271fe9bfdff155
Will Archer <will.archer@hotmail.com>
2023-04-03 13:54:03 +1000

    testing product top layout

--- src/components/header/banners/ProductBanners.js
+++ src/components/header/banners/ProductBanners.js
@@ -1,16 +1,20 @@
 import Styles from "../../../styles/productPage.module.css";
 import Nav from "../navigation/Nav";
 import ProductTags from "@/components/products/ProductTags";
-
+import ProductTopSection from "./../../templates/ProductTopSection";
 const ProductBanners = ({ productDetails }) => {
   return (
     <section className={Styles.pageBanner}>
       <Nav />
       <div className={Styles.pageBannerText}>
-        <h1>{productDetails.name}</h1>
-        <div className={Styles.metaContainer}>
-          <ProductTags productDetails={productDetails} />
-        </div>
+        <section className={Styles.titleMeta}>
+          <div className={Styles.titleMetaInner}>
+            <h1>{productDetails.name}</h1>
+            <div className={Styles.metaContainer}>
+              <ProductTags productDetails={productDetails} />
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,6 @@ export default function App({ Component, pageProps }) {
               nextProduct={pageProps.nextProduct}
             />
           )}
-
           <BoxedWidth>{page}</BoxedWidth>
           <Footer />
         </>
